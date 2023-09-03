2e7dc0565790d4987caedbb624a69cb0af3dc318
Aman Tyagi <amantyagi2k@gmail.com>
2023-09-03 16:58:48 +0530

    Questions and answers

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
-const obj = {
-  name: "Aman",
-  age: null,
-  gender: "male",
-  relationship: [],
+// 1 - Given a string, reverse each word in the sentence
+// eg: "i love javascript" ==> "i evol tpircsavaj"
+
+const str = "i love javascript";
+
+const strArr = str
+  .split(" ")
+  .map((word) => {
+    return word.split("").reverse().join("");
+  })
+  .join(" ");
+
+// console.log(strArr);
+
+// 2 - How to check if an object is an array or not?
+
+const checkArr = (arg) => {
+  if (Array.isArray(arg)) {
+    return "array";
+  } else {
+    return "not an array";
+  }
 };
 
-for (let a in obj) {
-  if (
-    obj[a] === "" ||
-    obj[a] === undefined ||
-    obj[a] === null ||
-    (Array.isArray(obj[a]) && obj[a].length < 0)
-  ) {
-    delete obj[a];
+// console.log(checkArr([])); // array
+// console.log(checkArr({})); // not an array
+
+// 3 - How to empty an array in Javascript without using any function or loops
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+arr.length = 0;
+// console.log(arr);
+
+// 4 - How would you check if a number is an interger or not?
+
+// 14.5 ==> 0.5
+
+// ** Check for an early return statement for non-numbers
+
+const isInterger = (num) => {
+  return num % 1 === 0;
+};
+
+// console.log(isInterger("123"));
+// console.log(isInterger(12.67));
+
+// 5 - Make this work :
+// duplicate([1,2,3,4,5]); [1,2,3,4,5,1,2,3,4,5]
+
+const duplicate = (arrNum) => {
+  return arrNum.concat(arrNum);
+};
+
+// console.log(duplicate([1, 2, 3, 4, 5]));
+
+// 6 - Write a Javascript function that reverse a number
+
+const num = -123456;
+function reverseNum(num) {
+  let rev = 0;
+  while (num) {
+    n = num % 10;
+    rev = rev * 10 + n;
+    num = parseInt(num / 10);
   }
+  return rev;
 }
+// console.log(reverseNum(-12345));
 
-delete obj["relationship"];
+// 9 - Write a Javascript function that accepts a string as a parameter and
+// converts the first letter of each word of the string in upper case
 
-console.log(obj);
+const convertToTitleCase = (str) => {
+  return str
+    .split(" ")
+    .map((word) => {
+      return word.substr(0, 1).toUpperCase() + word.substr(1);
+    })
+    .join(" ");
+};
+
+console.log(convertToTitleCase("lorem ipsum is a dummy text")); // "Lorem Ipsum Is A Dummy Text"
--- questions.md
+++ questions.md
@@ -1,4 +1,5 @@
 1 - Given a string, reverse each word in the sentence
+eg: "i love javascript" ==> "i evol tpircsavaj"
 
 2 - How to check if an object is an array or not? (Provide some code.)
 
@@ -17,7 +18,7 @@ duplicate([1,2,3,4,5]); [1,2,3,4,5,1,2,3,4,5]
 
 8 - Write a Javascript function that return a passed string with letters in alphabetical order
 
-9 - Write a Javascript functio that accepts a string as a parameter and converts the first letter of each word of the string in upper case
+9 - Write a Javascript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case
 
 10 - Write a Javascript function which accepts an argument and returns the type.
 
@@ -26,13 +27,33 @@ duplicate([1,2,3,4,5]); [1,2,3,4,5,1,2,3,4,5]
 ## LOOPS :
 
 12 - loop an array and add all members of it in an array of numbers and string, only add those members which are not strings.
+[1,2,3,4,5,6,7] (use only a js function)
 
 13 - loop an array of objects and remove all objects which don't have gender's value male
+
+[
+{name:"Aman",gender:"male"},
+{name:"Amit",gender:"male"},
+{name:"KuchBhi",gender:"non-binary"},
+{name:"Shubhi",gender:"female"}
+{name:"Nandini",gender:"female"}
+{name:"Parkhi",gender:"female"}
+{name:"Sukriti",gender:"female"}
+]
 
 ## Objects :
 
 14 - Remove key-value pair if the value is empty(null,undefined) or an empty array or object
 
+    const user = {
+        name : "Aman",
+        age : 23,
+        address : "Indiranagar",
+        bankAccountNumber : undefined,
+        pastHistory : [],
+        pastCompany : ""
+    }
+
 **Home-Work**
 
-Remove all the nested null objects from an object
+\*\*Remove all the nested null objects from an object (you have a data in database that contains empty object inside another object / you have a data with an array of objects some objects are null or do not have any key-value pair remove them all)
